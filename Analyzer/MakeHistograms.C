9aa16af5993e9e9d6e08ffc85f098907e9989446
taufiqismail1996 <109270321+taufiqismail1996@users.noreply.github.com>
2022-07-28 11:36:47 +0800

    Update MakeHistograms.C

--- Analyzer/MakeHistograms.C
+++ Analyzer/MakeHistograms.C
@@ -1,2 +1,7 @@
 void MakeHistograms(){
+  from ROOT.ROOT import RDataFrame
+df = RDataFrame("ntuple", "inputForMVATraining_CHS.root")
+h2 = df.Filter("dR2Mean")\
+       .Histo1D(("dR2Mean", "dR2Mean;dR2Mean Graph;Number of Entries"), "dR2Mean")
+h2.Draw()
 }
