414949d766bdc8ba96bb70c2f86b80abf7b5ddf6
taufiqismail1996 <109270321+taufiqismail1996@users.noreply.github.com>
2022-08-04 10:25:38 +0800

    Update MakeHistograms.C

--- Analyzer/MakeHistograms.C
+++ Analyzer/MakeHistograms.C
@@ -1,25 +1,24 @@
 void MakeHistograms(){
-  //traditional method
-  
+/*
+//traditional method
+
   TFile*f = new TFile("inputForMVATraining_CHS.root");
   TTree* t;
   f->GetObject("t",t);
   
   TCanvas* c1 = new TCanvas("c1","Histogram");
   t->Draw("dR2Mean");
+*/  
+
   
+//traditional method #2
   
-  
-  //traditional method #2
-  /*
   TCanvas *c1 = new TCanvas();
   TFile *input = new TFile("inputForMVATraining_CHS.root,"read");
                            TTree *tree = (TTree*)input->Get("tree");
-                           double dR2Mean, majW, min W;
+                           double dR2Mean;
                            tree->setBranchAddress("dR2Mean", &dR2Mean);
-                           tree->setBranchAddress("majW",&majW);
-                           tree->setBranchAddress("minW",&minW);
-                           
+                         
                            int entries = tree->GetEntries();
                            TH1F *hist = new TH1F("hist", "Histogram");
                            
@@ -29,7 +28,7 @@ void MakeHistograms(){
                              hist->Fill(x);
                            }
                            hist->Draw;
-                           
+/*                          
 //using RDataFrame
 df = RDataFrame("ntuple", "inputForMVATraining_CHS.root")
 h2 = df.Filter("dR2Mean")\
